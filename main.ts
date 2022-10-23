2a4729536b1d6c894f46e5fcb39279168fb3a24c
MargaretaTeacher <78282246+MargaretaTeacher@users.noreply.github.com>
2022-10-23 23:25:46 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,25 +1,59 @@
-let a = false
-basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        a = true
-        basic.showString("" + (a))
-        basic.showLeds(`
-            # # # # #
-            . . # . .
-            . . # . .
-            . . # . .
-            . . # . .
-            `)
+let bool = false
+let broj = 0
+input.onButtonPressed(Button.A, function () {
+    bool = true
+    basic.showString("" + (bool))
+    basic.showLeds(`
+        # # # # #
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.AB, function () {
+    broj = randint(1, 9)
+    basic.showNumber(broj)
+    if (broj % 2 == 0) {
+        basic.showIcon(IconNames.Yes)
+    } else {
+        basic.showIcon(IconNames.No)
     }
-    if (input.buttonIsPressed(Button.B)) {
-        a = false
-        basic.showString("" + (a))
+    basic.pause(100)
+})
+input.onButtonPressed(Button.B, function () {
+    bool = false
+    basic.showString("" + (bool))
+    basic.showLeds(`
+        . # # # .
+        . # . . .
+        . # # # .
+        . # . . .
+        . # . . .
+        `)
+})
+input.onLogoEvent(TouchButtonEvent.Touched, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
         basic.showLeds(`
-            . # # # .
-            . # . . .
-            . # # # .
-            . # . . .
-            . # . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+        basic.showString("T")
+        basic.showString("I")
+        basic.showString("S")
+        basic.showString("I")
+        basic.showString("N")
+        basic.showString("A")
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
             `)
     }
 })
