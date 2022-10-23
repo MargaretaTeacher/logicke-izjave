f7fe970ab78ec249576020c35d7aa9d081a417dc
MargaretaTeacher <78282246+MargaretaTeacher@users.noreply.github.com>
2022-10-23 23:29:28 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 let bool = false
-let broj = 0
 input.onButtonPressed(Button.A, function () {
     bool = true
     basic.showString("" + (bool))
@@ -11,16 +10,6 @@ input.onButtonPressed(Button.A, function () {
         . . # . .
         `)
 })
-input.onButtonPressed(Button.AB, function () {
-    broj = randint(1, 9)
-    basic.showNumber(broj)
-    if (broj % 2 == 0) {
-        basic.showIcon(IconNames.Yes)
-    } else {
-        basic.showIcon(IconNames.No)
-    }
-    basic.pause(100)
-})
 input.onButtonPressed(Button.B, function () {
     bool = false
     basic.showString("" + (bool))
@@ -31,29 +20,4 @@ input.onButtonPressed(Button.B, function () {
         . # . . .
         . # . . .
         `)
-})
-input.onLogoEvent(TouchButtonEvent.Touched, function () {
-    for (let index = 0; index < 4; index++) {
-        basic.showIcon(IconNames.Heart)
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.showString("T")
-        basic.showString("I")
-        basic.showString("S")
-        basic.showString("I")
-        basic.showString("N")
-        basic.showString("A")
-        basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-    }
 })
